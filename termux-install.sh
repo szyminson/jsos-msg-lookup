db6220f647fead4636db6a8527517267add41337
szyminson <szyminson@hotmail.com>
2020-10-05 20:47:38 +0200

    Added boot and install scripts for termux users

---
+++ termux-install.sh
@@ -0,0 +1,18 @@
+#!/data/data/com.termux/files/usr/bin/sh
+
+# Dependencies
+apt update & apt install python git tmux termux-api
+
+# Clone repo
+git clone https://github.com/szyminson/jsos-msg-lookup.git ~/jml
+
+# Python dependencies
+cd ~/jml
+pip install -r requirements.txt
+
+# Setup boot script
+mkdir -p ~/.termux/boot
+cp termux-boot-jml ~/.termux/boot
+
+# Done
+echo "Done!"
